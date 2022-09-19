dd881c207420dbbb99782e6110e8cae42c46fcdd
Sarvjot Singh <Sarvjot Singh@10.12.10.41>
2022-09-19 09:28:16 +0530

    Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,36 @@
+
+public class BubbleSort {  
+    static void bubbleSort(int[] arr) {  
+        int n = arr.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){   
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }    
+                 }  
+         } 
+    }
+
+         public static void main(String[] args) {  
+             int arr[] ={7,90,154,5,97,599,23};  
+              
+             System.out.println("Given Array: ");  
+             for(int i=0; i < arr.length; i++)
+             {  
+                     System.out.print(arr[i] + " ");  
+             }  
+             System.out.println();  
+               
+             bubbleSort(arr);
+              
+             System.out.println("Array After Bubble Sort: ");  
+             for(int i=0; i < arr.length; i++)
+             {  
+                     System.out.print(arr[i] + " ");  
+             }  
+
+     }  
+}  
